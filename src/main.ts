2549c1962030310e0e2f69367fb8127d89b40426
hcurtillet <hugo.curtillet@gmail.com>
2024-03-03 14:59:07 +0100

    Updates

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import helmet from 'helmet';
 import * as compression from 'compression';
-import process from 'process';
 
-console.log('process env', process.env);
 const PORT = 3000;
-console.log('PORT', PORT);
-console.log('database uri', process.env.DATABASE_URI);
-console.log('database name', process.env.DATABASE_NAME);
-console.log('database user', process.env.DATABASE_USER);
-console.log('database pass', process.env.DATABASE_PASS);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' });
