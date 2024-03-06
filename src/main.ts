ba9dbb72fe1b5de25c503df0414947d2290a2bd4
hcurtillet <hugo.curtillet@gmail.com>
2024-03-06 09:33:08 +0100

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import helmet from 'helmet';
 import * as compression from 'compression';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,7 +12,13 @@ async function bootstrap() {
   app.enableVersioning({ type: VersioningType.URI });
   app.use(helmet());
   app.use(compression());
+  const config = new DocumentBuilder()
+    .setTitle('Weekly menu API')
+    .setDescription('API for weekly menu')
+    .build();
   const PORT = 3000;
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
   await app.listen(PORT, () => {
     console.log(`🚀 Application running at port ${PORT}`);
   });
